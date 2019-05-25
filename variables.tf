a956a1b1937e342ec5eaf35b72cdca927cdef138
Ed Osorio <eosorio@users.noreply.github.com>
2019-05-25 15:57:10 -0500

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "do_token" {}
+
+variable "pvt" {
+  default = "/home/eduardo/.ssh/digitaloceank8s"
+}
+
+variable "default_port" {
+  default = "80"
+}
+
+variable "http_protocol" {
+  default = "http"
+}
